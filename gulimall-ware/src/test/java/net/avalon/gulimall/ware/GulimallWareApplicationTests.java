cab5b1c84569298f8b4ca2eec17e244eb1792284
Lacia <weiyinsama@gmail.com>
2023-03-05 22:00:18 +0800

    ware init

---
+++ gulimall-ware/src/test/java/net/avalon/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package net.avalon.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
